79c7702bb884ca00f2e7a25bb18b14791a538b0a
Ivan Avila <ivelaval@gmail.com>
2023-04-30 23:40:32 -0500

    nvim lua configurations were added

---
+++ nvim/lua/ivelaval/core/colorscheme.lua
@@ -0,0 +1,8 @@
+-- set colorschema to nightfly with protected call
+-- in case it isn't installed
+local status, _ = pcall(vim.cmd, "colorscheme dracula")
+if not status then
+  print("Colorscheme not found!") -- print error if colorscheme not installed
+  return
+end
+
